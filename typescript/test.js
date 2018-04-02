c63516ecafaad406ed58c5833ecb9aadbf0c174a
Richard <richy.liu.2002@gmail.com>
2018-04-01 21:39:14 -0700

    start typescript project

--- typescript/test.js
+++ typescript/test.js
@@ -1,11 +1,85 @@
-var Startup = /** @class */ (function () {
-    function Startup() {
+var Settings = /** @class */ (function () {
+    function Settings() {
     }
-    Startup.main = function () {
-        console.log('foo');
-        return 0;
-    };
-    return Startup;
+    Settings.debug = true;
+    return Settings;
 }());
-Startup.main();
+var Exception = /** @class */ (function () {
+    function Exception() {
+    }
+    Exception.warn = function (message) {
+        if (!Settings.debug)
+            console.warn(message);
+    };
+    return Exception;
+}());
+var Util = /** @class */ (function () {
+    function Util() {
+    }
+    Util.generateId = function () {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < 5; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    };
+    return Util;
+}());
+var Factory = /** @class */ (function () {
+    function Factory() {
+    }
+    Factory.prototype.Factory = function () {
+        this._id = Util.generateId();
+    };
+    Object.defineProperty(Factory.prototype, "id", {
+        get: function () {
+            return this._id;
+        },
+        set: function (newId) {
+            if (Settings.debug) {
+                this._id = newId;
+            }
+            else {
+                Exception.warn('Cannot set id (maybe use debug mode?)');
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Factory.prototype.generate = function () {
+    };
+    return Factory;
+}());
+var Bounds = /** @class */ (function () {
+    function Bounds() {
+    }
+    Bounds.prototype.Bounds = function () {
+    };
+    return Bounds;
+}());
+var PhysicalFactory = /** @class */ (function () {
+    function PhysicalFactory() {
+    }
+    PhysicalFactory.prototype.PhysicalFactory = function () {
+    };
+    return PhysicalFactory;
+}());
+var RectFactory = /** @class */ (function () {
+    function RectFactory() {
+    }
+    RectFactory.prototype.RectFactory = function (x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    };
+    return RectFactory;
+}());
+var EllipseFactory = /** @class */ (function () {
+    function EllipseFactory() {
+    }
+    EllipseFactory.prototype.CircleFactory = function () {
+    };
+    return EllipseFactory;
+}());
 //# sourceMappingURL=test.js.map
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,8 +1,82 @@
-class Startup {
-    public static main(): number {
-        console.log('foo');
-        return 0;
+class Settings {
+    public static debug: boolean = true;
+}
+
+class Exception {
+    public static warn(message: string) {
+        if (!Settings.debug)
+            console.warn(message);
     }
 }
 
-Startup.main();
+class Util {
+    public static generateId(): string {
+        let text: string = "";
+        const possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        
+        return text;
+    }
+}
+
+class Factory {
+    private _id: string;
+
+    public Factory() {
+        this._id = Util.generateId();
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(newId: string) {
+        if (Settings.debug) {
+            this._id = newId;
+        } else {
+            Exception.warn('Cannot set id (maybe use debug mode?)');
+        }
+    }
+
+    public generate(): Factory {
+        
+        return new Factory();
+    }
+}
+
+class Bounds {
+    public Bounds() {
+        
+    }
+}
+
+class PhysicalFactory {
+    
+    public PhysicalFactory() {
+        
+    }
+}
+
+
+class RectFactory {
+    public x: number;
+    public y: number;
+    public width: number;
+    public height: number;
+
+    public RectFactory(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+
+class EllipseFactory {
+    public CircleFactory() {
+
+    }
+}
